a559d2919c0c7d05e421fb3794261d08e0e740a8
Scott Harrison <scottrules44@gmail.com>
2023-03-07 22:07:38 -0600

    Updating Adapters for Max 11.8.1

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,9 +3,13 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { "AdColonyAdapter" },
-		frameworks = { "AdColony" },
-		frameworksOptional = {},
+
+		-- This is the name without the 'lib' prefix.
+		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
+		staticLibs = { "c++", "sqlite3", "z", "z.1.2.5", "AdColonyAdapter"},
+
+		frameworks = {"SafariServices", "JavaScriptCore", "CoreTelephony", "AdColony", "AdSupport"},
+		frameworksOptional = {"AppTrackingTransparency", "WebKit", "WatchConnectivity"},
 	},
 }
 
